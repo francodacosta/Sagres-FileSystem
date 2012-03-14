1c7537eee7f7a64d9b5055277e6155d4209fdcae
Nuno Costa <n.costa@efuture.nl>
2012-03-14 17:20:44 +0100

    improved documentation docbox docs

--- Sagres/Component/FileSystem/Exception/ResourceNotFoundException.php
+++ Sagres/Component/FileSystem/Exception/ResourceNotFoundException.php
@@ -1,6 +1,26 @@
 <?php
+/**
+ * This file is part of the Sagres package.
+ *
+ * (c) nuno costa <nuno@francodacosta.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package Sagres
+ */
 namespace Sagres\Component\FileSystem\Exception;
 
+/**
+ * Exception used when the resource is not available
+ *
+ * @package Sagres
+ * @subpackage FileSystem
+ *
+ * @author nuno costa <nuno@francodacosta.com>
+ * @since 10-11-2011
+ * @license MIT
+ */
 class ResourceNotFoundException extends \Exception
 {
 
